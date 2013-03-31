5e0778c44f4a49b55652dd35c4b28124ba6b2347
Tobias Wooldridge <tobias@wooldridge.id.au>
2013-03-31 18:03:24 +1030

    Bug fixin'.
    Most particularly, SerialReadInt16 didn't previously work (and was named
    wrong anyway). It didn't include anything, and didn't work. Now it does
    work.
    
    Also, "(Serial.read() << 8)" was a silly idea, given it just shifts the
    bits into oblivion as the datatype is only 8 bits long

--- splashpixel/splashpixel.ino
+++ splashpixel/splashpixel.ino
@@ -1,7 +1,7 @@
 // Copyright 2013 Tobias Wooldridge
 // Protocol originally based upon https://github.com/iKenndac/Arduino-Dioder-Playground
 
-#include "TLC5940.h"
+#include <Tlc5940.h>
 
 // Configure LEDs
 const int procolPreamble = 0xBA;
@@ -10,7 +10,7 @@ const int protocolInstructionVersion = 0x00;
 const int protocolInstructionSetColours = 0xBE;
 const int protocolInstructionPowerDown = 0xBF;
 
-const int protocolHeaderLength = 2;
+const int protocolHeaderLength = 4;
 const int protocolBodyLength = 12;
 const int protocolChecksumLength = 1;
 
@@ -22,70 +22,93 @@ byte receivedMessage[protocolBodyLength];
 
 long steppedUntil = millis();
 
-long messageStarted = millis();
+long messageStarted = 0;
 
 
 void setup() {
   Serial.begin(115200);
+  Tlc.init();  
 }
 
-int SerialReadInt16() {
-	(Serial.read() << 8) + Serial.read();
+
+unsigned int SerialReadUInt16() {
+  while (Serial.available() < 2) {}
+  unsigned int in = Serial.read();
+  in = (in << 8) + Serial.read();
+
+//  char buf[2];
+//  Serial.write("0x");
+//  Serial.write(itoa(in, buf, 16));
+//  Serial.write("\n");
+  
+  return in;  
 }
 
 
 void loop() {
-	while (Serial.available() >= protocolHeaderLength) {
-		messageStarted = millis();
+  while (Serial.available() >= protocolHeaderLength) {
+    messageStarted = millis();
 
-		// Sync
-		if (Serial.read() != procolPreamble)
-			continue;
+    // Sync
+    if (Serial.read() != procolPreamble) {
+      continue;
+    }
+    
+    Serial.write("Matched preamble\n");
 
-		byte instruction = Serial.read();
+    byte instruction = Serial.read();
+    unsigned int messageLength = SerialReadUInt16();
 
-		int messageLength = SerialReadInt16();
-
-		switch (instruction) {
-			case protocolInstructionSetColours: readColours(messageLength); break;
-			case protocolInstructionVersion: sendVersion(); break;
-			case protocolInstructionPowerDown: powerDown(); break;
-			
-			default: return;
-		}
-	}
-	
-	if (lastMessageHeader() > idleTimeout)
-	{
-		powerDown();
-	}
+    switch (instruction) {
+      case protocolInstructionSetColours: readColours(messageLength); break;
+//      case protocolInstructionVersion: sendVersion(); break;
+//      case protocolInstructionPowerDown: powerDown(); break;
+      default: ;
+    }
+    
+    Serial.write("Finished instruction\n");
+  }
+  
+  if (lastMessageHeader() > idleTimeout)
+  {
+    powerDown();
+  }
 }
 
 
 long lastMessageHeader() {
-	return millis() - messageStarted;
+  return millis() - messageStarted;
 }
 
-void readColours(int messageLength) {
-	if (messageLength % 4 != 0) {
-		return; // TODO add some clever error handling
-	}
-	int specifiedChannels = messageLength/4;
+void readColours(unsigned int messageLength) {
+  Serial.write("readColours\n");
+  
+  unsigned int specifiedChannels = messageLength/4;
+  
+  for (unsigned int i = 0; i < specifiedChannels; i++) {
+    unsigned int channel =   SerialReadUInt16();
+    unsigned int value =   SerialReadUInt16();
+    set(channel, value);
+  }
 
-	for (int i = 0; i < specifiedChannels; i++) {
-		Tlc.set(SerialReadInt16(), SerialReadInt16());
-	}
+  Tlc.update();
+}
+
+void set (unsigned int channel, unsigned int value) {
+    Tlc.set(channel, 4095 - value);
 }
 
 void powerDown() {
-	for (int i = 0; i < channelCount; i++) {
-		Tlc.set(i, 0);
-	}
+  for (int i = 0; i < channelCount; i++) {
+    set(i, 0);
+  }
+
+  Tlc.update();
 }
 
 void sendVersion() {
-	Serial.write(0x00); // Model
-	Serial.write(0x00); // Major version
-	Serial.write(0x01); // Minor version
+  Serial.write(0x00); // Model
+  Serial.write(0x00); // Major version
+  Serial.write(0x01); // Minor version
 }
 
